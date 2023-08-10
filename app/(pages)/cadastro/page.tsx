960cb601cd5e49a6fb2d27b4755bcf231a98ffd6
LucasVieiraS <lucasvieirasilva.lpta@gmail.com>
2023-08-10 12:23:19 -0300

    new(main): cadastro form

--- app/(pages)/cadastro/page.tsx
+++ app/(pages)/cadastro/page.tsx
@@ -2,44 +2,11 @@
 
 import Link from 'next/link';
 
-import Button from '@/app/components/Button';
 import Logo from '@/app/components/Logo';
-import { Form } from '@/app/components/Form';
 
-import { useForm } from 'react-hook-form';
-import { useRouter } from 'next/navigation';
-import { Input } from '@/components/ui/input';
-import { RxReload } from 'react-icons/rx';
-import { useState } from 'react';
-
-interface Login {
-  email: string;
-  password: string;
-  confirmpassword: string;
-}
+import RegisterForm from './register-form';
 
 export default function Login() {
-  const {
-    register,
-    handleSubmit,
-    watch,
-    formState: { errors },
-  } = useForm({
-    mode: 'onChange',
-    defaultValues: {
-      email: '',
-      password: '',
-      confirmpassword: '',
-    },
-  });
-
-  const onSubmit = (data: Login) => {
-    // Fazer request para conferir se já existe, se existir, avisar com toast, se não, avisar sucesso e logar.
-    console.log(data);
-  };
-
-
-  const router = useRouter();
 
   return (
     <div className="flex flex-row align-center justify-center w-[100vw] h-[100vh]">
@@ -54,91 +21,7 @@ export default function Login() {
             Tenha acesso a plataforma e torne o
             processo de gerenciamento de formulários mais ágil e prático.
           </p>
-          <Form.Root onSubmit={handleSubmit(onSubmit)}>
-            <Form.Label label="E-mail" />
-            <Input
-              autoComplete="email"
-              htmlFor="email"
-              error={errors.email}
-              placeholder="Entre com seu e-mail"
-              aria-invalid={errors.email ? 'true' : 'false'}
-              className="shadow"
-              onInvalid={(e: any) => {
-                e.preventDefault();
-              }}
-              {...register('email', {
-                required: 'E-mail é obrigatório',
-                validate: {
-                  matchPattern: (v) =>
-                    /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(v) ||
-                    'O endereço de e-mail deve ser válido',
-                },
-              })}
-              type="email"
-              id="email"
-            />
-            <Form.Error message={errors.email?.message} />
-
-            <Form.Label label="Senha" className="mt-2" />
-            <Input
-              htmlFor="password"
-              error={errors.password}
-              autoComplete="password"
-              placeholder="Entre com sua senha"
-              aria-invalid={errors.password ? 'true' : 'false'}
-              className="shadow"
-              onInvalid={(e: any) => {
-                e.preventDefault();
-              }}
-              {...register('password', {
-                required: 'Senha é obrigatória',
-                maxLength: {
-                  value: 30,
-                  message: 'Número máximo de caractéres é 30',
-                },
-                minLength: {
-                  value: 5,
-                  message: 'Número mínimo de caractéres é 5',
-                },
-              })}
-              type="password"
-              id="password"
-            />
-            <Form.Error message={errors.password?.message} />
-
-            <Form.Label label="Confirmar Senha" className="mt-2" />
-            <Input
-              htmlFor="confirmpassword"
-              error={errors.confirmpassword}
-              autoComplete="confirmpassword"
-              placeholder="Confirme a sua senha"
-              aria-invalid={errors.confirmpassword ? 'true' : 'false'}
-              className="shadow"
-              onInvalid={(e: any) => {
-                e.preventDefault();
-              }}
-              {...register('confirmpassword', {
-                required: 'Confirmar Senha é obrigatório',
-                validate: (val: string) => {
-                  if (watch('password') != val) {
-                    return 'As senhas não são iguais';
-                  }
-                },
-              })}
-              type="password"
-              id="confirmpassword"
-            />
-            <Form.Error message={errors.confirmpassword?.message} />
-
-            <Button
-              theme="dark-900"
-              size="full"
-              type="submit"
-              className="mt-4 flex items-center justify-center"
-            >
-              Cadastrar
-            </Button>
-          </Form.Root>
+          <RegisterForm />
           <div className="font-normal w-full text-center mt-6 flex justify-center">
             <p className="mr-1">Já possui uma conta? </p>
             <Link href="/login">
--- app/hooks/useAuthContext.ts
+++ app/hooks/useAuthContext.ts
@@ -1,100 +1,115 @@
 import Cookies from 'js-cookie';
 
-import axios from 'axios';
+import axios, { AxiosResponse } from 'axios';
 
-import B64Encrypt from './useBase64'
+import B64Encrypt from './useBase64';
 import { ToastWrapper } from '../utils/ToastWrapper';
 import { useRouter } from 'next/navigation';
 
 const tokenCookieKey = 'login_token';
 
-const registerPath = `/auth/register`
-const loginPath = `/auth/login`
+const registerPath = `/auth/register`;
+const loginPath = `/auth/login`;
 
 const instance = axios.create({
-  baseURL: `http://localhost:8080`,
+  baseURL: `http://10.68.20.127:8080`,
 });
 
-// We create dupes cause they're the same (for now)
-interface LoginData {
-  usuario: string,
-  senha: string,
+export interface CustomResponse extends AxiosResponse {
+  errors?: string[];
 }
 
-interface RegisterData {
-  usuario: string,
-  senha: string,
+// We create dupes cause they're the same (for now)
+export interface LoginData {
+  usuario: string;
+  senha: string;
 }
+
+export interface RegisterData {
+  usuario: string;
+  senha: string;
+}
+
+const B64EncryptObject = B64Encrypt();
 
 const useAuthHandler = () => {
-
   const router = useRouter();
 
   const login = (loginData?: LoginData) => {
-
     const succesfullyAuthenticated = () => {
       ToastWrapper.success('Login realizado com sucesso!');
       return router.push('/dashboard');
-    }
+    };
 
-    const unsuccesfullyAuthenticated = (message: string = 'Não foi possível realizar o login.') => {
+    const unsuccesfullyAuthenticated = (
+      message: string = 'Não foi possível realizar o login.'
+    ) => {
       ToastWrapper.error(message);
-    }
+    };
 
-    const withCookie = () => {
-      const B64EncryptObject = B64Encrypt()
-
+    const getDecodedJSON = () => {
       const tokenCookie = Cookies.get(tokenCookieKey);
-      const decodedCookie = B64EncryptObject.decodeText(tokenCookie)
+      const decodedCookie = B64EncryptObject.decodeText(tokenCookie);
       if (!decodedCookie) {
         return;
       }
 
       const decodedJSON = JSON.parse(decodedCookie);
-      if (!decodedJSON) {
-        return;
-      }
+      return decodedJSON;
+    }
+
+    const withCookie = () => {
+      const decodedJSON = getDecodedJSON()
+      if (!decodedJSON) return;
 
       const requestParams = {
         params: {
-          ...decodedJSON
+          ...decodedJSON,
         },
-      }
+      };
 
-      instance.get(loginPath, requestParams)
-        .then((response) => {
+      instance
+        .get(loginPath, requestParams)
+        .then((response: CustomResponse) => {
           console.log(response);
           if (response.status == 200) {
             succesfullyAuthenticated();
           } else {
             // checar mensagem de erro
+            if (!response.errors) {
+              return unsuccesfullyAuthenticated();
+            }
             unsuccesfullyAuthenticated();
           }
         })
         .catch((error) => {
-          // tratar erro
-          unsuccesfullyAuthenticated("Ocorreu um erro na autenticação, tente novamente em breve...")
+          if (!error.message) return;
+          unsuccesfullyAuthenticated(error.message);
         });
-    }
+    };
 
     const withNewLogin = () => {
       const requestParams = {
         params: {
-          ...loginData
+          ...loginData,
         },
-      }
+      };
       console.log(requestParams);
-      instance.post(loginPath, requestParams)
+      instance
+        .post(loginPath, requestParams)
         .then((response) => {
           console.log(response);
           if (response.status == 200) {
-            Cookies.set(tokenCookieKey, response.headers.login_token, { expires: 30 })
+            Cookies.set(tokenCookieKey, response.headers.login_token, {
+              expires: 30,
+            });
           }
         })
         .catch((error) => {
-          console.log(error);
-        })
-    }
+          if (!error.message) return;
+          unsuccesfullyAuthenticated(error.message);
+        });
+    };
 
     const hasAuthTokenInCookies = Cookies.get(tokenCookieKey);
 
@@ -103,29 +118,33 @@ const useAuthHandler = () => {
     } else {
       withNewLogin();
     }
-  }
+  };
 
   const register = (registerData: RegisterData) => {
     const registerParams = {
       params: {
-        ...registerData
+        ...registerData,
       },
-    }
-    instance.post(registerPath, registerParams)
+    };
+    instance
+      .post(registerPath, registerParams)
       .then((response) => {
         if (response.status == 200) {
-          Cookies.set(tokenCookieKey, response.headers.login_token, { secure: true })
+          // previously response.headers.login_token
+          Cookies.set(tokenCookieKey, 'o hash do cookie them q ir aqui', {
+            secure: true,
+          });
         }
       })
       .catch((error) => {
         console.log(error);
       });
-  }
+  };
 
   return {
     login,
-    register
-  }
-}
+    register,
+  };
+};
 
 export default useAuthHandler;
