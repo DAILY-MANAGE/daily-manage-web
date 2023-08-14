c0bb24625078dc2b0af8ce748709da87feaabe52
LucasVieiraS <lucasvieirasilva.lpta@gmail.com>
2023-08-14 12:25:01 -0300

    new(main): div structure

--- app/(pages)/formularios/[id]/page.tsx
+++ app/(pages)/formularios/[id]/page.tsx
@@ -4,7 +4,17 @@ export default function IdFuncionario({ params }: { params: { id: number } }) {
   return (
     <>
       <title>{`Formulário ${params.id} | Daily Manage`}</title>
-      <p>{params.id}</p>
+      <div className="flex-col flex">
+
+        <div className="flex-1 space-y-4 p-8 pt-6">
+          <div className="flex items-center justify-between space-y-2 flex-col md:flex-row">
+            <h2 className="text-3xl font-bold tracking-tight">{`Formulário ${params.id}`}</h2>
+            <div className="flex items-center space-x-2">
+
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
