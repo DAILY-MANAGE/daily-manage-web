fd5cd903211eb5c8953b40493b8ac911bef8932e
LucasVieiraS <lucasvieirasilva.lpta@gmail.com>
2023-08-14 12:15:42 -0300

    fix(main): remove obsolete import

--- app/components/Form/index.tsx
+++ app/components/Form/index.tsx
@@ -1,11 +1,9 @@
 import FormError from './FormError';
-import FormInput from './FormInput';
 import FormLabel from './FormLabel';
 import FormRoot from './FormRoot';
 
 export const Form = {
   Root: FormRoot,
   Label: FormLabel,
-  Input: FormInput,
   Error: FormError,
 };
