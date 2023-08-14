6f623993a36c38710c9f86dc4065e6f13c743744
LucasVieiraS <lucasvieirasilva.lpta@gmail.com>
2023-08-14 12:17:32 -0300

    chore(main): remove useless metadata

--- app/(pages)/formularios/[id]/page.tsx
+++ app/(pages)/formularios/[id]/page.tsx
@@ -1,25 +1,9 @@
 'use client';
 
-import { Metadata } from 'next';
-
-type Props = {
-  params: { id: string };
-  searchParams: { [key: string]: string | string[] | undefined };
-};
-
-export async function generateMetadata({
-  params,
-  searchParams,
-}: Props): Promise<Metadata> {
-  return {
-    title: `Formulário ${params.id} | Daily Manage`,
-  };
-}
-
 export default function IdFuncionario({ params }: { params: { id: number } }) {
   return (
     <>
-      <title>{params.id}</title>
+      <title>{`Formulário ${params.id} | Daily Manage`}</title>
       <p>{params.id}</p>
     </>
   );
