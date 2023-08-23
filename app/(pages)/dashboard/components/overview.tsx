b63f22422b3a15ff7cdb08c137274b9da09af033
Lucas <lucasvieirasilva.lpta@gmail.com>
2023-08-22 21:58:40 -0300

    chore(main): remove $

--- app/(pages)/dashboard/components/overview.tsx
+++ app/(pages)/dashboard/components/overview.tsx
@@ -69,7 +69,7 @@ export function Overview() {
           fontSize={12}
           tickLine={false}
           axisLine={false}
-          tickFormatter={(value) => `$${value}`}
+          tickFormatter={(value) => `${value}`}
         />
         <Bar dataKey="total" fill="#15F45F" radius={[4, 4, 0, 0]}/>
       </BarChart>
