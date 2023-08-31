fe539042aba8b92807cb67d733dd85fd7a62288f
Lucas <lucasvieirasilva.lpta@gmail.com>
2023-08-30 22:27:49 -0300

    refactor(main): editorconfig

--- app/components/Shadcn/input.tsx
+++ app/components/Shadcn/input.tsx
@@ -12,7 +12,7 @@ const Input = React.forwardRef<HTMLInputElement, any>(
         type={type}
         className={cn(
           'flex h-10 w-full rounded-md border border-black/50 bg-background px-3 shadow py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none ring-0 focus-visible:border-2 focus-visible:ring-ring data-[invalid=true]:outline-red-700 outline outline-0 data-[invalid=true]:outline-1 focus-visible:ring-offset-0 disabled:cursor-not-allowed disabled:opacity-50',
-          className
+          className 
         )}
         ref={ref}
         {...props}
