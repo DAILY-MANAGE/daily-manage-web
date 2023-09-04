e3cc8ec23a204ca2896c319b8f217d80485d5b40
Lucas <lucasvieirasilva.lpta@gmail.com>
2023-09-03 22:43:29 -0300

    refactor: cleanup

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,35 +1,35 @@
-import './globals.scss';
-import { Inter } from 'next/font/google';
-import { ToastContainer } from 'react-toastify';
-import { ToastWrapper } from './utils/ToastWrapper';
-import { Custom } from './components/Custom';
+import './globals.scss'
+import { Inter } from 'next/font/google'
+import { ToastContainer } from 'react-toastify'
+import { ToastWrapper } from './utils/ToastWrapper'
+import NavRoot from './components/Navbar/nav-root'
 
 const inter = Inter({
   weight: ['300', '400', '500', '600', '700'],
   subsets: ['latin'],
-});
+})
 
 export const metadata = {
   title: 'Daily Manage',
   description: 'Aplicação que auxilia na criação de formulários rotineiros',
-};
+}
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode;
+  children: React.ReactNode
 }) {
   if (typeof window !== 'undefined') {
-    ToastWrapper.setupWindow(window);
+    ToastWrapper.setupWindow(window)
   }
 
   return (
     <html lang="pt-br">
       <body className={`${inter.className} bg-light-100 dark:bg-zinc-900`}>
         <ToastContainer icon={true} />
-        <Custom.NavRoot />
+        <NavRoot />
         {children}
       </body>
     </html>
-  );
+  )
 }
