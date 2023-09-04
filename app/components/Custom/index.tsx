80ef1da96ed058d21d2b9568d65c3993396c81ec
Lucas <lucasvieirasilva.lpta@gmail.com>
2023-09-03 22:44:05 -0300

    refactor: clean garbage

--- app/components/Custom/index.tsx
+++
@@ -1,9 +0,0 @@
-import Button from './button';
-import Logo from './logo';
-import NavRoot from './Navbar/navroot';
-
-export const Custom = {
-  Button: Button,
-  Logo: Logo,
-  NavRoot: NavRoot,
-};
--- app/hooks/useFetch.ts
+++
@@ -1,9 +0,0 @@
-import { useEffect } from 'react';
-
-const useFetch = () => {
-  const get = (payload: any, callback: any) => {};
-
-  return {
-    get,
-  };
-};
