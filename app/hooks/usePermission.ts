92cbef5de3580d89239cf93cc320aa173f06c1af
LucasVieiraS <lucasvieirasilva.lpta@gmail.com>
2023-11-01 09:00:25 -0300

    chore: change perms by user

--- app/hooks/usePermission.ts
+++ app/hooks/usePermission.ts
@@ -1,9 +1,18 @@
 import { useEffect, useState } from "react"
 import { VER_TODAS_PERMISSOES_DO_USUARIO_LOGADO_NA_EQUIPE, VER_TODAS_PERMISSOES_POR_EQUIPE } from "../utils/EndpointStorage"
+import { useAuth } from "./useAuth"
 import { useFetch } from "./useFetch"
 
 export const usePermission = (permission: string | undefined = undefined, equipeId: number, usuario?: string) => {
   const [contains, setContains] = useState(false)
+
+  const {session} = useAuth()
+
+  if (!usuario) {
+    if (session) {
+      usuario = session.usuario
+    }
+  }
 
   const { data, refetch } = useFetch({
     url: usuario ? VER_TODAS_PERMISSOES_POR_EQUIPE.replace("{usuarioId}", usuario) : VER_TODAS_PERMISSOES_DO_USUARIO_LOGADO_NA_EQUIPE,
