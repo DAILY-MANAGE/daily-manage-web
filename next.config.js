923c52b131388b309914469727cb3abf00266cdb
LucasVieiraS <lucasvieirasilva.lpta@gmail.com>
2023-11-16 08:05:58 -0300

    chore: clean dependencies

--- next.config.js
+++ next.config.js
@@ -4,8 +4,7 @@ const nextConfig = {
     domains: ['source.unsplash.com'],
   },
   experimental: {
-    appDir: true,
-    serverComponentsExternalPackages: ["@prisma/client", "bcryptjs"]
+    serverComponentsExternalPackages: []
   },
 };
 
